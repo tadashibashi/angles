ac824a2605144a02ceeddbc355a8c47119620180
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2024-04-23 06:11:30 -0700

    fix angles header, missing defs in egl angle extensions

--- include/angles.h
+++ include/angles.h
@@ -1,10 +1,9 @@
 #pragma once
+#include <KHR/khrplatform.h>
+
 #include <EGL/egl.h>
-#include <EGL/eglext_angle.h>
 #include <EGL/eglext.h>
 #include <EGL/eglplatform.h>
 
 #include <GLES3/gl3.h>
 #include <GLES3/gl3platform.h>
-
-#include <KHR/khrplatform.h>
